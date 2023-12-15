d1d4a165a02c00e3aac7979aa790297ff2fa1d3e
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-12-16 01:24:58 +0300

    Update cofffee_cafe_karpov_test.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- Python/karpov_courses/cofffee_cafe_karpov_test.py
+++ Python/karpov_courses/cofffee_cafe_karpov_test.py
@@ -33,30 +33,27 @@
 # milk = 2
 # visitors = 35
 
-coffee = 1  # запас молотого кофе в кофейне (в килограммах)
-milk = 2  # запас молока в кофейне (в литрах)
+coffee *= 1000
+milk *= 1000
+visitors = 1
 
-coffee_gram = coffee * 1000  # переводим килограммы кофе в граммы
-milk_ml = milk * 1000  # переводим литры молока в миллилитры
-
-espresso_coffee = 7  # количество кофе для эспрессо (в граммах)
-latte_milk = 180  # количество молока для латте (в миллилитрах)
-cappuccino_milk = 100  # количество молока для капучино (в миллилитрах)
-visitors = 0  # количество посетителей, которых может обслужить кофейня
-
-while coffee_gram >= espresso_coffee and milk_ml >= cappuccino_milk:
+while True:
+    if visitors % 3 == 0:
+        if milk >= 100 and coffee >= 7:
+            coffee -= 7
+            milk -= 100
+        else:
+            break
+    elif visitors % 5 == 0:
+        if milk >= 180 and coffee >= 7:
+            coffee -= 7
+            milk -= 180
+        else:
+            break
+    else:
+        if coffee >= 7:
+            coffee -= 7
+        else:
+            break
     visitors += 1
-    coffee_gram -= espresso_coffee
-    if visitors % 3 == 0 and visitors % 5 != 0:
-        milk_ml -= cappuccino_milk
-    if visitors % 5 == 0 and visitors % 3 != 0:
-        milk_ml -= latte_milk
-    if visitors % 3 == 0 and visitors % 5 == 0:
-        milk_ml -= cappuccino_milk
-#print(visitors)
-if milk_ml == 0:
-    if visitors % 5 != 0 and visitors % 3 != 0:
-           visitors += 1
-    if visitors % 5 != 0 or visitors % 3 != 0:
-           visitors += 1
-print(visitors)
+visitors -= 1
